5d7ad22378c6da0f515d75d596452a8792500c15
King Sam <141226873+Samue-l1@users.noreply.github.com>
2024-03-17 14:21:20 +0300

    Add files via upload

---
+++ Classicbot/mediafire.js
@@ -0,0 +1,41 @@
+// ⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈
+//▮CLASSIC BOT INC 2024 ®️ALL RIGHTS RESERVED
+//▮
+//▮FORK AND DON'T FORGET TO GIVE A STAR
+//▮
+//▮THIS SOFTWARE IS UNDER UZ COPYRIGHT
+//▮
+//▮REPORT ABUSE OF THIS SOFTWARE EMAIL US
+//▮samuelcircute@gmail.com
+//▮WHATSAPP US : +258 86 910 3969
+//▮YOUTUBE CHANNELL: https://youtube.com/@SamuelCircute?si=cVq02S23tGxSiPjO
+//▮
+//╰▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+//
+//┏━━━━━━━━━━━━━━━━━━━━━━━━━
+//┃THIS SOFTWARE INCLUDES 
+//┃SOME ENCRYPTED FILES
+//┃
+//┃THANKS FOR CHOOSING CLASSIC BOT
+//┃I WROTE THIS SCRIPT BY MYSELF THIS SCRIPT IS FOR EVERYONE DONT SELL IT
+//┗━━━━━━━━━━━━━━━━━━━━━━━━━
+//
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const mediafireDl = async (url) => {
+const res = await axios.get(url) 
+const $ = cheerio.load(res.data)
+const hasil = []
+const link = $('a#downloadButton').attr('href')
+const size = $('a#downloadButton').text().replace('Download', '').replace('(', '').replace(')', '').replace('\n', '').replace('\n', '').replace('                         ', '')
+const seplit = link.split('/')
+const nama = seplit[5]
+mime = nama.split('.')
+mime = mime[1]
+hasil.push({ nama, mime, size, link })
+return hasil
+}
+
+
+module.exports = { mediafireDl }
